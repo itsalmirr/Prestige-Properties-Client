ea55dec7dc9a4cddcbfd06b21edef7f861cc193d
gl1tchh0x <itsalmirr@icloud.com>
2025-02-19 14:21:47 -0500

    navbar animation added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import { FaBars, FaXmark } from 'react-icons/fa6'
-import { Dialog, DialogPanel } from '@headlessui/react'
+import { Dialog } from '@headlessui/react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { Button } from './ui/button'
@@ -76,58 +77,76 @@ const Navbar = () => {
           </Link>
         </div>
       </nav>
-      <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className='lg:hidden'
-      >
-        <div className='fixed inset-0 z-50' />
-        <DialogPanel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
-          <div className='flex items-center justify-between'>
-            <a href='#' className='-m-1.5 p-1.5'>
-              <span className='sr-only'>Your Company</span>
-              <Image
-                width={32}
-                height={32}
-                alt=''
-                src='https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600'
-                className='h-8 w-auto'
-              />
-            </a>
-            <button
-              type='button'
-              onClick={() => setMobileMenuOpen(false)}
-              className='-m-2.5 rounded-md p-2.5 text-gray-700'
+
+      <AnimatePresence>
+        {mobileMenuOpen && (
+          <Dialog
+            static
+            open={true}
+            onClose={setMobileMenuOpen}
+            className='lg:hidden'
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className='fixed inset-0 z-50 bg-black/50'
+            />
+
+            <motion.div
+              initial={{ x: '100%' }}
+              animate={{ x: 0 }}
+              exit={{ x: '100%' }}
+              transition={{ type: 'tween', duration: 0.3 }}
+              className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'
             >
-              <span className='sr-only'>Close menu</span>
-              <FaXmark aria-hidden='true' className='size-6' />
-            </button>
-          </div>
-          <div className='mt-6 flow-root'>
-            <div className='-my-6 divide-y divide-gray-500/10'>
-              <div className='space-y-2 py-6'>
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold dark:text-gray-200 text-gray-900 hover:bg-gray-50'
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-              <div className='py-6'>
-                <Link
-                  href='/'
-                  className='-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold dark:text-gray-200 text-gray-900 hover:bg-gray-50'
+              <div className='flex items-center justify-between'>
+                <a href='#' className='-m-1.5 p-1.5'>
+                  <span className='sr-only'>Your Company</span>
+                  <Image
+                    width={32}
+                    height={32}
+                    alt=''
+                    src='https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600'
+                    className='h-8 w-auto'
+                  />
+                </a>
+                <button
+                  type='button'
+                  onClick={() => setMobileMenuOpen(false)}
+                  className='-m-2.5 rounded-md p-2.5 text-gray-700'
                 >
-                  Log in
-                </Link>
+                  <span className='sr-only'>Close menu</span>
+                  <FaXmark aria-hidden='true' className='size-6' />
+                </button>
               </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
+              <div className='mt-6 flow-root'>
+                <div className='-my-6 divide-y divide-gray-500/10'>
+                  <div className='space-y-2 py-6'>
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold dark:text-gray-200 text-gray-900 hover:bg-gray-50'
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                  <div className='py-6'>
+                    <Link
+                      href='/auth/signin'
+                      className='-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold dark:text-gray-200 text-gray-900 hover:bg-gray-50'
+                    >
+                      Log in
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </Dialog>
+        )}
+      </AnimatePresence>
     </header>
   )
 }
