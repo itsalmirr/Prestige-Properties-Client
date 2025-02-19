bd24eaf67d70f640dfd76eae34569538e6248ba5
gl1tchh0x <itsalmirr@icloud.com>
2025-02-19 05:31:23 -0500

    ui improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,9 +11,9 @@ const Header = () => {
         <div
           style={{
             clipPath:
-              'polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)',
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
-          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-burgundy to-gold opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-burgundy to-gold opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
         />
       </div>
       <div className='mx-auto max-w-2xl py-32 sm:py-48'>
@@ -56,7 +56,7 @@ const Header = () => {
         <div
           style={{
             clipPath:
-              'polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)',
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
           className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#E43D00] to-[#FFE900] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
         />
