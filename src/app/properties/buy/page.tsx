1dad4788b8898859a2774f0e8e30ca23483142f8
i0sys <i0sys@proton.me>
2025-03-27 13:47:26 -0400

    Add Footer component and integrate next-view-transitions; enhance FeaturedListings with realtor details

--- src/app/properties/buy/page.tsx
+++ src/app/properties/buy/page.tsx
@@ -1,7 +1,12 @@
+import FeaturedListings from '@/components/FeaturedListings'
 import React from 'react'
 
 const BrowseListings = () => {
-  return <div>BrowseListings</div>
+	return (
+		<div>
+			<FeaturedListings />
+		</div>
+	)
 }
 
 export default BrowseListings
--- src/components/FeaturedListings.tsx
+++ src/components/FeaturedListings.tsx
@@ -3,6 +3,7 @@ import api from '@/lib/api'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { MdEmail, MdPhone } from 'react-icons/md'
+import { Button } from './ui/button'
 
 interface Listing {
 	title: string
@@ -13,6 +14,13 @@ interface Listing {
 	bedroom: number
 	bathroom: number
 	description: string
+	edges: {
+		realtor: {
+			full_name: string
+			phone: string
+			email: string
+		}
+	}
 	media: {
 		url: string
 	}[]
@@ -32,8 +40,9 @@ const FeaturedListings = () => {
 		fetchProperties()
 	}, [])
 
+	console.log(listings)
 	return (
-		<div className="mx-10 py-24 sm:py-32">
+		<div className="mx-10">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="mx-auto max-w-2xl lg:mx-0">
 					<h2 className="text-4xl font-semibold tracking-tight text-pretty text-primary sm:text-5xl">
@@ -59,26 +68,29 @@ const FeaturedListings = () => {
 								{listing.title.substring(0, 27)}
 							</h3>
 							<p className="p-2 text-base/7 text-primary">{listing.address}</p>
-							<ul className="ml-2 mt-6 flex gap-x-6">
+							<ul className="ml-2 mt-6 flex flex-col md:flex-row gap-4 md:gap-x-6">
 								<li>
 									<a
 										href={listing.city}
-										className="text-gray-400 hover:text-gray-500"
+										className="text-gray-400 hover:text-gray-500 flex items-center gap-2"
 									>
-										<span className="sr-only">X</span>
+										<span className="sr-only">Realtor phone</span>
 										<MdPhone className="size-5" />
+										<span>{listing.edges.realtor.phone}</span>
 									</a>
 								</li>
 								<li>
 									<a
 										href={listing.state}
-										className="text-gray-400 hover:text-gray-500"
+										className="text-gray-400 hover:text-gray-500 flex items-center gap-2"
 									>
-										<span className="sr-only">LinkedIn</span>
+										<span className="sr-only">Realtor Email</span>
 										<MdEmail className="size-5" />
+										<p>{listing.edges.realtor.email}</p>
 									</a>
 								</li>
 							</ul>
+							<Button className="w-full mt-4">Make an Inquiry</Button>
 						</li>
 					))}
 				</ul>
